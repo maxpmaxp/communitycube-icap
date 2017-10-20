ed3f696fabb80db7e1092c0589c0dd8bb2a9d5c7
Maksym Polshcha <maxp@sterch.net>
2017-10-20 13:29:46 +0300

    Transfer-* headers added

--- icapserver.py
+++ icapserver.py
@@ -5,7 +5,12 @@ from pyicap import BaseICAPRequestHandler
 
 class ICAPHandler(BaseICAPRequestHandler):
 
-    istag = "v0.1"
+    version = "v0.1a"
+    ignore_files = ('gif', 'jpg', 'png', 'jpeg', 'txt', 'pdf', 'js', 'exe', 'bin', 'tiff', 'ttf', 'svg',
+                    'woff2', 'woff', 'mpeg', 'mp3', 'mp4', 'avi', 'wav', 'aac', 'flac', 'wma', 'vox',
+                    'raw', 'dmg', 'webm', 'mkv', 'vob', 'ogg', 'ogv', 'drc', 'gifv', 'mng', 'qt',
+                    'wmv', 'yuv', 'rm', 'rmvb', 'asf', 'amv', 'm4p', 'm4v', 'mpg', 'mp2', 'mpeg', 'mpe', 'mpv',
+                    'm2v', 'svi', '3gp', '3g2', 'mxf', 'roq', 'nsv', 'flv', 'f4v', 'f4p', 'f4a', 'f4b')
 
     # Simple echo-style server icap://icap.myorganization.com/echo
 
@@ -13,7 +18,7 @@ class ICAPHandler(BaseICAPRequestHandler):
         self.set_icap_response(200)
         self.set_icap_header('Methods', 'RESPMOD')
         self.set_icap_header('Preview', '0')
-        self.set_icap_header('ISTag', self.istag)
+        self.set_icap_header('ISTag', self.version)
         self.send_headers(False)
 
     def echo_RESPMOD(self):
@@ -26,7 +31,9 @@ class ICAPHandler(BaseICAPRequestHandler):
         self.set_icap_header('Methods', 'RESPMOD')
         self.set_icap_header('ISTag', self.istag)
         self.set_icap_header('Preview', '0')
-        self.send_headers(False)
+        self.set_icap_header('Transfer-Ignore', ', '.join(self.ignore_files))
+        self.set_icap_header('Transfer-Preview', '*')
+
 
     def communitycube_menu_RESPMOD(self):
         self.no_adaptation_required()
