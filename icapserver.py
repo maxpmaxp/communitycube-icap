82edd9a9804e6f0f7b5f1bb11c102c848c138ade
Maksym Polshcha <maxp@sterch.net>
2017-10-20 14:13:00 +0300

    use bytes

--- icapserver.py
+++ icapserver.py
@@ -5,20 +5,21 @@ from pyicap import BaseICAPRequestHandler
 
 class ICAPHandler(BaseICAPRequestHandler):
 
-    version = "v0.1a"
-    ignore_files = ('gif', 'jpg', 'png', 'jpeg', 'txt', 'pdf', 'js', 'exe', 'bin', 'tiff', 'ttf', 'svg',
-                    'woff2', 'woff', 'mpeg', 'mp3', 'mp4', 'avi', 'wav', 'aac', 'flac', 'wma', 'vox',
-                    'raw', 'dmg', 'webm', 'mkv', 'vob', 'ogg', 'ogv', 'drc', 'gifv', 'mng', 'qt',
-                    'wmv', 'yuv', 'rm', 'rmvb', 'asf', 'amv', 'm4p', 'm4v', 'mpg', 'mp2', 'mpeg', 'mpe', 'mpv',
-                    'm2v', 'svi', '3gp', '3g2', 'mxf', 'roq', 'nsv', 'flv', 'f4v', 'f4p', 'f4a', 'f4b')
+    version = b'v0.1a'
+    ignore_files = (b'gif', b'jpg', b'png', b'jpeg', b'txt', b'pdf', b'js', b'exe', b'bin', b'tiff', b'ttf', b'svg',
+                    b'woff2', b'woff', b'mpeg', b'mp3', b'mp4', b'avi', b'wav', b'aac', b'flac', b'wma', b'vox',
+                    b'raw', b'dmg', b'webm', b'mkv', b'vob', b'ogg', b'ogv', b'drc', b'gifv', b'mng', b'qt',
+                    b'wmv', b'yuv', b'rm', b'rmvb', b'asf', b'amv', b'm4p', b'm4v', b'mpg', b'mp2', b'mpeg', b'mpe',
+                    b'mpv', b'm2v', b'svi', b'3gp', b'3g2', b'mxf', b'roq', b'nsv', b'flv', b'f4v', b'f4p', b'f4a',
+                    b'f4b')
 
     # Simple echo-style server icap://icap.myorganization.com/echo
 
     def echo_OPTIONS(self):
         self.set_icap_response(200)
-        self.set_icap_header('Methods', 'RESPMOD')
-        self.set_icap_header('Preview', '0')
-        self.set_icap_header('ISTag', self.version)
+        self.set_icap_header(b'Methods', b'RESPMOD')
+        self.set_icap_header(b'Preview', b'0')
+        self.set_icap_header(b'ISTag', self.version)
         self.send_headers(False)
 
     def echo_RESPMOD(self):
@@ -28,11 +29,11 @@ class ICAPHandler(BaseICAPRequestHandler):
 
     def communitycube_menu_OPTIONS(self):
         self.set_icap_response(200)
-        self.set_icap_header('Methods', 'RESPMOD')
-        self.set_icap_header('ISTag', self.version)
-        self.set_icap_header('Preview', '0')
-        self.set_icap_header('Transfer-Ignore', ', '.join(self.ignore_files))
-        self.set_icap_header('Transfer-Preview', '*')
+        self.set_icap_header(b'Methods', b'RESPMOD')
+        self.set_icap_header(b'ISTag', self.version)
+        self.set_icap_header(b'Preview', b'0')
+        self.set_icap_header(b'Transfer-Ignore', b', '.join(self.ignore_files))
+        self.set_icap_header(b'Transfer-Preview', b'*')
 
 
     def communitycube_menu_RESPMOD(self):
